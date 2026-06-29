13f7939a2e77b1adfc32accf3bf7e4cffe8cfe89
apnacollege-demo <shresthamohanty05@gmail.com>
2026-06-29 15:28:09 +0530

    Day2 level done

---
+++ 02_Day_Variables_builtin_functions/exercise_level1.py
@@ -0,0 +1,34 @@
+# Inside 30DaysOfPython create a folder called day_2. Inside this folder create a file named variables.py
+# 2. Write a python comment saying 'Day 2: 30 Days of python programming'
+# 3. Declare a first name variable and assign a value to it
+# 4. Declare a last name variable and assign a value to it
+# 5. Declare a full name variable and assign a value to it
+# 6. Declare a country variable and assign a value to it
+# 7. Declare a city variable and assign a value to it
+# 8. Declare an age variable and assign a value to it
+# 9. Declare a year variable and assign a value to it
+# 10. Declare a variable is_married and assign a value to it
+# 11. Declare a variable is_true and assign a value to it
+# 12. Declare a variable is_light_on and assign a value to it
+# 13. Declare multiple variable on one line
+
+first_name = "Shrestha"
+last_name = "Mohanty"
+full_name = "Shrestha Mohanty"
+country = "India"
+city = "Cuttack"
+age = 20
+year =2026
+is_true = True
+is_light_on = True
+
+
+first_name, last_name, full_name, country, city, age , year, is_true, is_light_on = "Shrestha" , "Mohanty" , "Shrestha Mohanty" , "India" , "Cuttack", 20, 2026, True, True
+
+print("First name: " , first_name)
+print("Full name : " , full_name) 
+print("Country: " , country)
+print("City: " , city)
+print("Age: " , age)
+print("Year: " , year)
+print("Is light on ? : ", is_light_on) 
