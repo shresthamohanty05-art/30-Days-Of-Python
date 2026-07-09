5061d278a3b951650258c64f1131b0a9379c7501
apnacollege-demo <shresthamohanty05@gmail.com>
2026-07-09 23:52:46 +0530

    day 3 done

--- 03_Day_Operators/Exercises.py
+++ 03_Day_Operators/Exercises.py
@@ -100,15 +100,31 @@ var = 2+3j
 # 12. Find the length of 'python' and 'dragon' and make a falsy comparison statement.
 a = "python"
 b = "dragon"
-print("Length of python: " , len(a))
-print("Length of dragon: ", len(b))
-print(a==b)
+# print("Length of python: " , len(a))
+# print("Length of dragon: ", len(b))
+# print(a==b)
 # 13. Use _and_ operator to check if 'on' is found in both 'python' and 'dragon'
+print("on" in "python" and "on" in "dragon" )
 # 14. _I hope this course is not full of jargon_. Use _in_ operator to check if _jargon_ is in the sentence.
+
+print("_jargon_" in "_I hope this course is not full of jargon_")
 # 15. There is no 'on' in both dragon and python
+
+print(not("on" in "python" and "on" in "dragon") )
+
 # 16. Find the length of the text _python_ and convert the value to float and convert it to string
+print(len("_python_"))
 # 17. Even numbers are divisible by 2 and the remainder is zero. How do you check if a number is even or not using python?
+num = 8
+print(num%2 == 0)
 # 18. Check if the floor division of 7 by 3 is equal to the int converted value of 2.7.
+print(7//3)
 # 19. Check if type of '10' is equal to type of 10
+print('10' == 10)
 # 20. Check if int('9.8') is equal to 10
+print(int(float('9.8')) == 10 )
 # 21. Write a script that prompts the user to enter hours and rate per hour. Calculate pay of the person?
+hour = int(input("Enter total hours: "))
+rate = int(input("Enter the rate: "))
+pay = hour*rate
+print("Pay : " , pay)
