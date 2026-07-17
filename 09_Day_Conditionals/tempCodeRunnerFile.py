061b8a7fd7ed3c764f73f4816b63a1ed5cf6a0a5
apnacollege-demo <shresthamohanty05@gmail.com>
2026-07-17 15:05:12 +0530

    day 9 done

---
+++ 09_Day_Conditionals/tempCodeRunnerFile.py
@@ -0,0 +1,12 @@
+mark = int(input("Enter your marks: "))
+
+# if mark>=90 and mark<100 :
+#     print('A')
+# elif mark>=80 and mark<90 :
+#     print('B')
+# elif mark>=70 and mark<80 :
+#     print('C')
+# elif mark>=60 and mark<70 :
+#     print('D')
+# else:
+#     print('F')
